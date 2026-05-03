36c325032d2f774acf32af7d23da6587c24811fa
relderyw <reldery1422@gmail.com>
2026-05-02 20:00:39 -0400

    Chore: Clean up temporary test scripts

--- scratch.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-function test() {
-    const html = fs.readFileSync('drafted_raw.html', 'utf8');
-    // Find timestamps. App router JSON usually contains strings like "2026-05-02T..." or unix epoch
-    const isoDates = html.match(/202[4-9]-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d{3})?Z?/g) || [];
-    console.log("ISO Dates found:", new Set(isoDates));
-}
-test();
